a413a4929cc29ceed07a3b035784fc9662ec704a
DanniTan96 <det278@nyu.edu>
2023-09-11 19:50:56 -0400

    Create plot_gaussian.py

---
+++ ps-1/plot_gaussian.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+def plotter():
+	x = np.sort(np.random.uniform(-10, 10, 1000))
+	mean = 0
+	std = 3.0
+
+	y = (1/(std*np.sqrt(2*np.pi)))*np.exp(-0.5*((x/std)**2))
+
+	plt.plot(x, y, 'bo')
+	plt.xlabel("X")
+	plt.ylabel("Y")
+	plt.savefig("gaussian.png")
+
+if __name__ == "__main__":
+	plotter()
